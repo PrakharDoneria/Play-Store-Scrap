8bd053454fee58bc106f4c6c763a198c629d4c26
Prakhar Doneria <gamesprotec@gmail.com>
2024-08-24 10:31:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+import play_scraper
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def redirect_to_dev():
+    return redirect("https://play.google.com/store/apps/dev?id=8237023983569180393")
+
+@app.route('/app', methods=['GET'])
+def app_details():
+    app_id = request.args.get('id')
+    if not app_id:
+        return jsonify({"error": "App ID is required"}), 400
+
+    try:
+        app_details = play_scraper.details(app_id)
+        return jsonify(app_details)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
